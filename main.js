22ef3f9858eaf5f5ca9f583dfacbd2087541dae5
Lovekesh Pal <lovekeshfgr@gmail.com>
2021-12-20 00:01:18 +0530

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Now we have all of our constants */
 const display1El = document.querySelector(".display-1");
 const display2El = document.querySelector(".display-2");
 const tempResultEl = document.querySelector(".temp-result");
@@ -7,12 +8,15 @@ const equalEl = document.querySelector(".equal");
 const clearAllEl = document.querySelector(".all-clear");
 const clearLastEl = document.querySelector(".last-entity-clear");
 
-let dis1Num = "";
-let dis2Num = "";
-let result = null;
-let lastOperation = "";
+//Variables
+let dis1Num = ""; //Initially it will be empty
+let dis2Num = ""; //Initially it will be empty
+let result = null; //It will be null
+let lastOperation = ""; //Last operation,initially empty
 let haveDot = false;
+//If we have dot (point) in our number, it will take care of that. This will be used because we can not have two points within the same number. for eg. 23.3.3 <-- this is not a valid number
 
+//Adding event listener to every number, Number Display and Dot (point) functionality
 numbersEl.forEach((number) => {
   number.addEventListener("click", (e) => {
     if (e.target.innerText === "." && !haveDot) {
@@ -21,34 +25,38 @@ numbersEl.forEach((number) => {
       return;
     }
     dis2Num += e.target.innerText;
-    display2El.innerText = dis2Num;
+    display2El.innerText = dis2Num; //upon click on any number it will show us in display
   });
 });
 
+//For Mathematical Operations
 operationEl.forEach((operation) => {
   operation.addEventListener("click", (e) => {
     if (!dis2Num) return;
-    haveDot = false;
+    //check if we have any number in display to perform operation, if yes then we will go further otherwise we will return.
+    haveDot = false; //Setting haveDot to false, we are allowing new dot in our new number.
     const operationName = e.target.innerText;
     if (dis1Num && dis2Num && lastOperation) {
+      //If we have these three, then only move further
       mathOperation();
     } else {
       result = parseFloat(dis2Num);
     }
     clearVar(operationName);
-    lastOperation = operationName;
+    lastOperation = operationName; //Whenevr we do any operation, it will update
     console.log(result);
   });
 });
 
 function clearVar(name = "") {
-  dis1Num += dis2Num + " " + name + " ";
+  dis1Num += dis2Num + " " + name + " "; //Updating the display number 1
   display1El.innerText = dis1Num;
-  display2El.innerText = "";
-  dis2Num = "";
-  tempResultEl.innerText = result;
+  display2El.innerText = ""; //Clearing Display
+  dis2Num = ""; //Clearing variable
+  tempResultEl.innerText = result; // For showing temporary result
 }
 
+//Functionality for Math Operations
 function mathOperation() {
   if (lastOperation === "X") {
     result = parseFloat(result) * parseFloat(dis2Num);
@@ -62,6 +70,7 @@ function mathOperation() {
     result = parseFloat(result) % parseFloat(dis2Num);
 }
 
+//For equal
 equalEl.addEventListener("click", (e) => {
   if (!dis2Num || !dis2Num) return;
   haveDot = false;
@@ -73,6 +82,7 @@ equalEl.addEventListener("click", (e) => {
   dis1Num = "";
 });
 
+//For Clearing All
 clearAllEl.addEventListener("click", (e) => {
   display1El.innerText = "";
   display2El.innerText = "";
@@ -81,6 +91,8 @@ clearAllEl.addEventListener("click", (e) => {
   result = "";
   tempResultEl.innerText = "";
 });
+
+// For clearing last entry
 clearLastEl.addEventListener("click", (e) => {
   display2El.innerText = "";
   dis2Num = "";
@@ -88,6 +100,7 @@ clearLastEl.addEventListener("click", (e) => {
 
 //Adding Keyboard Functionality
 
+//To see which key is pressed
 window.addEventListener("keydown", (e) => {
   if (
     e.key === "0" ||
@@ -111,9 +124,12 @@ window.addEventListener("keydown", (e) => {
     clickEqual();
   } else if (e.key == "Backspace") {
     clickClear();
+  } else if (e.key == "Delete") {
+    clickClearLast();
   }
 });
 
+//Functions for pressing diffent combinations
 function clickButtonEl(key) {
   numbersEl.forEach((button) => {
     if (button.innerText === key) {
@@ -135,4 +151,7 @@ function clickEqual() {
 }
 function clickClear() {
   clearAllEl.click();
+}
+function clickClearLast() {
+  clearLastEl.click();
 }
